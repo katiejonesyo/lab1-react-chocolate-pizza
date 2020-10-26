e373a8e0a7f3f4c15ff213462cb4c51e5c6086c3
Katelinn Jones <katiejonesyo@gmail.com>
2020-10-26 14:54:02 -0700

    pizzaaaa

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import Chocolatepizza from './Chocolatepizza.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={head.png} className="head" alt="head" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
